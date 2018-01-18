966b79a62b2e0e4f501ba5ce43cf4e5c42535ee2
Niklas M <mikkelsenniklas@gmail.com>
2018-01-18 15:12:52 +0100

    Added for loop exercises project

---
+++ For loops Csharpskolan/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace For_loops_Csharpskolan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Övning 1
+            //for (int i = 0; i < 101; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Övning 2
+            //for (int i = 20; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Övning 3
+            //for (int i = 0; i < 52; i++)
+            //{
+            //    if (i % 2 == 0)
+            //        Console.WriteLine(i);
+            //}
+
+            for (int i = 0; i < 52; i += 2)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
